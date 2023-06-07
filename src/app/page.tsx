be71a516274dc1d426d63783c65221b8f9ccad28
Muhammad Hassan Ejaz <shaikhhassan164@gmail.com>
2023-06-07 23:02:35 +0500

    Nav completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import Nav from '../../src/app/components/nav';
+
+
 
 export default function Home() {
   return (
     <div>
-      <Nav/>
+     
     </div>
   )
 }
