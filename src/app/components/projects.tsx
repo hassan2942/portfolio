48da5c7be47b983578951a79e098848a9e4fe0df
Muhammad Hassan Ejaz <shaikhhassan164@gmail.com>
2023-06-09 02:23:22 +0500

    .

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Projects() {
   return (
@@ -20,12 +20,11 @@ export default function Projects() {
           <div className="flex flex-wrap -m-4">
             <div className="lg:w-1/3 sm:w-1/2 p-4">
               <div className="flex relative">
-                <Image
+                {/* <Image
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={require("../../../public/assets/projects/General-Election-piece.jpg")}
-                
-                />
+                /> */}
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 hover:cursor-pointer">
                   <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
                     FYP PROJECT
@@ -40,8 +39,8 @@ export default function Projects() {
                   </p>
                   <Link href={"http://evotingblockchain3.surge.sh/"}>
                     <p className="text-blue-600 hover:underline hover:text-black">
-                    View Project..
-                  </p>
+                      View Project..
+                    </p>
                   </Link>
                 </div>
               </div>
