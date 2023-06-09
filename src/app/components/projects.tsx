c55276e150eccd8b0a29471d89fc8ed3562d2b0a
Muhammad Hassan Ejaz <shaikhhassan164@gmail.com>
2023-06-09 18:30:28 +0500

    issue resolved now adding projects

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Projects() {
   return (
@@ -14,13 +15,13 @@ export default function Projects() {
           heard of them man bun deep jianbing selfies heirloom.
         </p>
       </div>
-      {/* <div className="flex flex-wrap -m-4">
+      <div className="flex flex-wrap -m-4">
         <div className="lg:w-1/3 sm:w-1/2 p-4">
           <div className="flex relative">
-            <img
+            <Image
               alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/600x360"
+              className="absolute inset-0 w-full h-full object-cover object-center "
+              src={require("../../../public/assets/projects/General-Election-piece.jpg")}
             />
             <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
               <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
@@ -36,112 +37,8 @@ export default function Projects() {
             </div>
           </div>
         </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/601x361"
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                The Catalyzer
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/603x363"
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                The 400 Blows
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/602x362"
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                Neptune
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/605x365"
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                Holden Caulfield
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/606x366"
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                Alper Kamu
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div> */}
+     
+      </div>
     </div>
   </section>
   </div>
