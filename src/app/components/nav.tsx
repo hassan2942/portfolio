b2b5ef57412497f0c08790445cc77bf3c056df19
Muhammad Hassan Ejaz <shaikhhassan164@gmail.com>
2023-06-09 23:01:36 +0500

    skills responsiveness updated

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -36,7 +36,7 @@ export default function Nav() {
               CONTACT
             </Link>
           </nav>
-          <a href="/assets/resume/Muhammad Hassan Resume.pdf">
+          <a href="/assets/resume/Muhammad Hassan Resume.pdf" target="_blank">
             <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
               Download CV
               <FaDownload className="text-xl ml-2"/>
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 
 export default function Projects() {
   return (
-    <div >
+    <div>
       <section className="text-gray-600 body-font" id="project">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900" >
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               PROJECTS
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
               In my project section, you will find a diverse range of innovative
               and impactful projects that showcase my skills and passion for
-              problem-solving. From web development to UX/UI
-              design, each project reflects my dedication to delivering
-              exceptional results and exceeding client expectations.
+              problem-solving. From web development to UX/UI design, each
+              project reflects my dedication to delivering exceptional results
+              and exceeding client expectations.
             </p>
           </div>
           <div className="flex flex-wrap -m-4">
@@ -29,20 +29,23 @@ export default function Projects() {
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    Voting Project
+                    REPLICA
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    E-Voting System Using Blockchain Technology
+                    FACEBOOK CLONE
                   </h1>
                   <p className="leading-relaxed">
-                    This is a project for organizations who select their highly
-                    qualified candidate on the basis of election and they value
-                    security and anonymity.
+                    I designed and developed a Facebook clone app, using Nextjs.
+                    The app replicates the core features of Facebook, such as
+                    user authentication.
+                    {/* news 
+                     feed, posting, commenting, and real-time notifications,
+                    providing a seamless and interactive user experience.  */}
                   </p>
                   <Link
                     href={"http://evotingblockchain3.surge.sh/"}
                     target="_blank"
-                    className="hover:text-blue-600 hover:font-bold "
+                    className="font-semibold text-black hover:text-blue-600 hover:font-bold "
                   >
                     <p>View Project</p>
                   </Link>
@@ -53,25 +56,25 @@ export default function Projects() {
               <div className="flex relative">
                 <Image
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center border-gray-900 border-4"
+                  className=" absolute inset-0 w-full h-full object-cover object-center border-gray-900 border-4"
                   src={require("../../../public/assets/projects/todo.jpg")}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    Voting Project
+                    TO-DO LIST PROJECT
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    E-Voting System Using Blockchain Technology
+                    TO-DO LIST
                   </h1>
                   <p className="leading-relaxed">
-                    This is a project for organizations who select their highly
-                    qualified candidate on the basis of election and they value
-                    security and anonymity.
+                    I developed a minimalist to-do list app using Next.js,
+                    allowing users to efficiently manage their tasks
+                    and stay organized.
                   </p>
                   <Link
                     href={"http://evotingblockchain3.surge.sh/"}
                     target="_blank"
-                    className="hover:text-blue-600 hover:font-bold "
+                    className="font-semibold text-black hover:text-blue-600 hover:font-bold "
                   >
                     <p>View Project</p>
                   </Link>
@@ -100,7 +103,7 @@ export default function Projects() {
                   <Link
                     href={"http://evotingblockchain3.surge.sh/"}
                     target="_blank"
-                    className="hover:text-blue-600 hover:font-bold "
+                    className="font-semibold text-black hover:text-blue-600 hover:font-bold  "
                   >
                     <p>View Project</p>
                   </Link>
@@ -108,7 +111,6 @@ export default function Projects() {
               </div>
             </div>
           </div>
-           
         </div>
       </section>
     </div>
