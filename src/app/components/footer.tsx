770ad1a3a023f4f9366e23208b94796364c3594c
Muhammad Hassan Ejaz <shaikhhassan164@gmail.com>
2023-06-16 23:19:47 +0500

    github/linkedin links updated

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { AiFillFacebook , AiOutlineInstagram , AiFillTwitterCircle ,AiFillLinkedin} from "react-icons/ai";
+import {
+  AiFillFacebook,
+  AiOutlineInstagram,
+  AiFillGithub,
+  AiFillLinkedin,
+} from "react-icons/ai";
 import Link from "next/link";
 
 export default function Footer() {
@@ -14,18 +19,28 @@ export default function Footer() {
             © 2023 KARACHI - PAKISTAN —
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <Link 
-            target="_blank"
-            href={"https://www.facebook.com/shaikh.hassain/"}>
+            <Link
+              target="_blank"
+              href={"https://www.facebook.com/shaikh.hassain/"}
+            >
               <AiFillFacebook className="text-2xl hover:text-[blue]" />
             </Link>
-         <Link href={"https://www.instagram.com/Yarhassaan/"}>
-            <AiOutlineInstagram className="text-2xl ml-2 hover:text-[#c03737]"/>
-         </Link>
-
-        <AiFillTwitterCircle className="text-2xl ml-2 hover:text-[#47a7fc]"/>
-
-        <AiFillLinkedin className="text-2xl ml-2 hover:text-[#2f40ff]"/>
+            <Link href={"https://www.instagram.com/Yarhassaan/"}>
+              <AiOutlineInstagram className="text-2xl ml-2 hover:text-[#c03737]" />
+            </Link>
+            <Link 
+            target="_blank"
+            href={"https://github.com/hassan2942"}>
+              <AiFillGithub className="text-2xl ml-2 hover:text-[#47a7fc]" />
+            </Link>
+            <Link
+              target="_blank"
+              href={
+                "https://www.linkedin.com/in/muhammad-hassan-ejaz-0588271b7/"
+              }
+            >
+              <AiFillLinkedin className="text-2xl ml-2 hover:text-[#2f40ff]" />
+            </Link>
           </span>
         </div>
       </footer>
